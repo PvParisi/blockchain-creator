6948b14aad871fc0b79a898105840fe751371268
Piervincenzo Parisi <piervi_parisi@yahoo.it>
2018-04-27 18:28:17 +0200

    styling WIP

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import './App.css';
 
 import Block from './Components/Block';
 import NewBlock from './Components/NewBlock';
+import Header from './Components/Header';
 
 //import Blockchain from './blockchain/Blockchain';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Block index='0'/>
+      <div >
+        <Header />
         <NewBlock />
+        <Block index='0'/>
       </div>
     );
   }
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+	return(
+		<div className='tc'>
+            <h1>BLOCKCHAIN</h1>
+            <h3>Create a fully featured blockchain</h3>
+		</div>
+	);
+}
+
+export default Header;
