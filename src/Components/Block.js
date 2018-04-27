83a4a479d0cdbfe2a7507edb4004dc2e2a9de38c
Piervincenzo Parisi <piervi_parisi@yahoo.it>
2018-04-27 15:00:36 +0200

    material kit

--- src/Components/Block.js
+++ src/Components/Block.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const Block = ({ index, previousHash, timestamp, data, hash, nonce }) => {
+	const cardDivStyle = {width: '20rem'};
+
 	return(
-		<div>Block</div>
+		<div className='card' style={cardDivStyle}>
+			<div className='card-body'>
+				<h4 className='card-title'>Block</h4>
+			</div>
+		</div>
 	);
 }
 
